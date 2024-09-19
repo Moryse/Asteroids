7f3ea1aff7f2a89e7e6a197eb7afb2fb6d1dff9b
codingruslan@gmail.com <codingruslan@gmail.com>
2024-09-18 20:12:55 -0700

    Asteroids progress

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
     print("Starting asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = clock.tick(60) / 1000
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color = "black")
+            
+        player.update(dt)
+    
+            
+        screen.fill("black")
+        player.draw(screen)
+
         pygame.display.flip()
+        
+    pygame.tick(60)
 
 if __name__ == "__main__":
     main()
